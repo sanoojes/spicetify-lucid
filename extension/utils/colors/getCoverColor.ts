ddbbc70d13f1c03f01a0ce46dc44969dcaf09822
Sanooj Es <sanoojes6371@gmail.com>
2025-11-10 00:18:21 +0530

    remove unnecessary comments

--- extension/utils/colors/getCoverColor.ts
+++ extension/utils/colors/getCoverColor.ts
@@ -16,13 +16,11 @@ export async function getCoverColor(imageUrl: string): Promise<ExtractedColor | 
 
   if (colorData && !colorData.isFallback) {
     return {
-      // Convert the Color object to the "#abcdef" format
       colorRaw: { hex: colorData.colorRaw.toCSS(Spicetify.Color.CSSFormat.HEX), fallback: false },
       colorDark: { hex: colorData.colorDark.toCSS(Spicetify.Color.CSSFormat.HEX), fallback: false },
       colorLight: { hex: colorData.colorLight.toCSS(Spicetify.Color.CSSFormat.HEX), fallback: false }
     };
   }
 
-  // Else, return undefined
   return undefined;
 }
